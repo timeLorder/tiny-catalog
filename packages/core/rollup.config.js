3edc9ee9a55c0eb80e38940d17197512b7e0519a
timeLorder <lyh9134wl@gmail.com>
2021-02-27 22:06:27 +0800

    feat: improve rollup config

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,12 +1,71 @@
-import babel from "@rollup/plugin-babel";
-import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import babel from '@rollup/plugin-babel';
+import typescript2 from 'rollup-plugin-typescript2';
+import tempDir from 'temp-dir';
+import pkg from './package.json';
+const { join } = require('path');
 
-export default {
-  input: "src/index.ts",
-  output: [
-    { name: "TinyCatalogCore", file: pkg.main, format: "cjs" },
-    { name: "TinyCatalogCore", file: pkg.module, format: "es" },
-  ],
-  plugins: [babel(), typescript()],
+const cwd = process.cwd();
+const name = 'TinyCatalogCore';
+const tsOpts = {
+  cwd,
+  clean: true,
+  cacheRoot: `${tempDir}/.rollup_plugin_typescript2_cache`,
+  tsconfig: join(cwd, 'tsconfig.json'),
+  tsconfigOverride: {
+    compilerOptions: {
+      // Support dynamic import
+      target: 'esnext',
+    },
+  },
+  check: true,
 };
+const getBabelOpts = type => ({
+  presets: [
+    require.resolve('@babel/preset-typescript'),
+    [
+      require.resolve('@babel/preset-env'),
+      {
+        targets: { browsers: ['last 2 versions', 'IE 10'] },
+        modules: type === 'es' ? false : 'auto',
+      },
+    ],
+  ],
+  plugins: [
+    require.resolve('@babel/plugin-proposal-optional-chaining'),
+    [
+      require.resolve('@babel/plugin-proposal-class-properties'),
+      { loose: true },
+    ],
+    ...(type === 'es'
+      ? [
+          [
+            require.resolve('@babel/plugin-transform-runtime'),
+            {
+              useESModules: true,
+              version: require('@babel/runtime/package.json').version,
+            },
+          ],
+        ]
+      : []),
+  ],
+  babelHelpers: type === 'es' ? 'runtime' : 'bundled',
+  exclude: /\/node_modules\//,
+  babelrc: false,
+  extensions: ['.js', '.jsx', '.ts', '.tsx', '.es6', '.es', '.mjs'],
+});
+
+const rollupConfig = [
+  {
+    input: 'src/index.ts',
+    output: { name, file: pkg.main, format: 'cjs' },
+    plugins: [typescript2(tsOpts), babel(getBabelOpts('cjs'))],
+  },
+  {
+    input: 'src/index.ts',
+    output: { name, file: pkg.module, format: 'es' },
+    plugins: [typescript2(tsOpts), babel(getBabelOpts('es'))],
+    external: [/@babel\/runtime/],
+  },
+];
+
+export default rollupConfig;
